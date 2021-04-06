b429243c1ee5283f9036b9596d8d76e43b22947d
carl6227 <78572664+carl6227@users.noreply.github.com>
2021-04-06 15:37:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ $("#connectBtn").on('click', function () {
         client.subscribe($('#subTopic').val())
     })
     $("#pub-button").on('click', () => {
-        client.subscribe($('#subTopic').val())
+
         client.publish($('#pubTopic').val(), $('#pubMessage').val())
-        $('.messageWrapper').append('<div class="col-sm-8 float-right">  <p class="payload sendWrapper">'+$('#pubMessage').val()+'</p><p class="text">'+"you"+'<span>'+new Date().toLocaleTimeString()+'</span>'+'</p></div>')
+        $('.messageWrapper').append(' <div class="col-sm-8 float-right">  <p class="payload sendWrapper">'+$('#pubMessage').val()+'</p> <p class="text">'+"you"+'<span class="span">'+new Date().toLocaleTimeString()+' </span> </p>  <img class="user1" src="profile.png"></div> ')
         $('#pubMessage').val("");
         $('#img').hide()
     })
@@ -28,7 +28,7 @@ pubTopic = $("#pubTopic").val();
 message = $("#pubMessage").val();
 client.on('message', function (pubTopic, message) {
     $('#img').hide();
-    $('.messageWrapper').append(' <div class="col-sm-8 float-left" > <p class="payload recieveWrapper">' + message.toString() + '</p><p class="text">' + pubTopic +'<span >'+new Date().toLocaleTimeString() +'</span></p></div>')
+    $('.messageWrapper').append(' <div class="col-sm-8 float-left" > <img  class="user2"src="man.png"><p class="payload recieveWrapper">' + message.toString() + '</p><p class="text2">' + pubTopic +'<span class="float-right">'+new Date().toLocaleTimeString() +'</span></p> </div>')
 })
 
 
