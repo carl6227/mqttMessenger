8e21f01c1a92d6a57aee58d60be8dab0c7721b2f
unknown <2ndyrGroupA@PNC1-09.pn.local>
2021-04-14 10:30:15 +0800

    fixed

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 
 var client = mqtt.connect($('#broker').val())
- ;
+
+;
 $("#connectBtn").on('click', function () {
     $("#status").val('Connecting...')
     client.on('connect', function () {
-     
+
         $("#status").val('Connected!')
-       
+
     })
-    $('#subTopic').on('change',function(){
+    $('#subTopic').on('change', function () {
         client.subscribe($('#subTopic').val())
     })
     $("#pub-button").on('click', () => {
-
         client.publish($('#pubTopic').val(), $('#pubMessage').val())
-        $('.messageWrapper').append(' <div class="col-sm-8 float-right">  <p class="payload sendWrapper">'+$('#pubMessage').val()+'</p> <p class="text">'+"you"+'<span class="span">'+new Date().toLocaleTimeString()+' </span> </p>  <img class="user1" src="profile.png"></div> ')
         $('#pubMessage').val("");
         $('#img').hide()
+        
     })
-    $("#clear").on('click',()=>{
-        $('.messageWrapper div').empty();   
+    $("#clear").on('click', () => {
+        $('.messageWrapper div').empty();
         $('#pubMessage').val("");
         $('#img').show();
     })
 })
-pubTopic = $("#pubTopic").val();
+
+subTopic = $("#subTopic").val();
 message = $("#pubMessage").val();
-client.on('message', function (pubTopic, message) {
+client.on('message', function (subTopic, message) {
     $('#img').hide();
-    $('.messageWrapper').append(' <div class="col-sm-8 float-left" > <img  class="user2"src="man.png"><p class="payload recieveWrapper">' + message.toString() + '</p><p class="text2">' + pubTopic +'<span class="float-right">'+new Date().toLocaleTimeString() +'</span></p> </div>')
+    now= new Date().toLocaleTimeString();
+    if (subTopic == $("#pubTopic").val()) {
+        $('.messageWrapper').append(' <div class="col-sm-8 float-right  "><img class="user1" src="profile.png">  <p class=" sendWrapper">' + message.toString() + '</p>  <p class="text">' + subTopic + '|<span>' +now.substr(0,now.length-2) + ' </span> </p> </div> ')
+    } else {
+        $('.messageWrapper').append(' <div class="col-sm-8 float-left" > <img  class="user2"src="man.png"><p class="recieveWrapper">' + message.toString() + '</p><p class="text2">' + subTopic + '|<span >' + now.substr(0,now.length-2) + '</span></p> </div>')
+    }
 })
 
 
